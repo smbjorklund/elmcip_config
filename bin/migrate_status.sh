b0edc98f8b2e99731790963ba3ee958abd5ad16a
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-08-29 15:37:31 +0200

    Get all migration status

--- bin/migrate_status.sh
+++ bin/migrate_status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Collect status.
+# Collect migration status from all ELMCIP data.
 
 # Import updated config.
 #../vendor/bin/drush config:import --partial --source="modules/custom/uib_humanists/config/install" --yes
 
 cd web || exit 1
-../vendor/bin/drush
+../vendor/bin/drush --version || exit 1
 
-../vendor/bin/drush migrate:status --group=uib_files
+../vendor/bin/drush migrate:status --group=uib_nodes,uib_files,uib_taxonomy,uib_comments,uib_users,uib_paragraphs
